afd19ef4cd1bbec3c9d046e14d876a92dbe9d278
Hillary Houston <hhouston823@gmail.com>
2024-07-19 11:35:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const scriptURL = 'Your_Add_URL' // Write your own app script url here
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Thank You For Subscribing!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
